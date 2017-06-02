bc24e674aad36e61e14455d351bc910320517f1d
MartkCz <antik494@gmail.com>
2017-06-02 11:48:41 +0200

    added assertThrownException

--- src/TUnitTest.php
+++ src/TUnitTest.php
@@ -16,4 +16,22 @@ trait TUnitTest {
 		parent::setUp();
 	}
 
+	public function assetThrownException(callable $function, $class, $message = NULL, $code = NULL) {
+		$e = NULL;
+		try {
+			call_user_func($function);
+		} catch (\Exception $e) {
+		}
+
+		if ($e === NULL) {
+			$this->fail("$class was expected, but none was thrown");
+		} elseif (!$e instanceof $class) {
+			$this->fail("$class was expected but got " . get_class($e) . ($e->getMessage() ? " ({$e->getMessage()})" : ''));
+		} elseif ($message && $message !== $e->getMessage()) {
+			$this->fail("$class with a message matching {$message} was expected but got {$e->getMessage()}");
+		} elseif ($code !== NULL && $e->getCode() !== $code) {
+			$this->fail("$class with a code {$code} was expected but got {$e->getCode()}");
+		}
+	}
+
 }
