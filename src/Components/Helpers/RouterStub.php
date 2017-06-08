58f2fef1e9479a56e21b353a9939a6a4ed96a001
MartkCz <antik494@gmail.com>
2017-06-08 19:37:45 +0200

    removed strict_types

--- src/Components/Helpers/RouterStub.php
+++ src/Components/Helpers/RouterStub.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace WebChemistry\Testing\Components\Helpers;
 
 use Nette;
--- src/Components/Requests/PresenterRequest.php
+++ src/Components/Requests/PresenterRequest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace WebChemistry\Testing\Components\Requests;
 
 use Nette\Application\IPresenter;
