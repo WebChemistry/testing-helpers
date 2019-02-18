d5fad835e963d8f750c77a8cc26c8a096a72196a
MartkCz <antik494@gmail.com>
2019-02-18 10:53:39 +0100

    ObjectMixins is deprecated

--- src/TMagicGet.php
+++ src/TMagicGet.php
@@ -10,8 +10,21 @@ trait TMagicGet {
 	 * @param string $name
 	 * @return mixed
 	 */
-	public function __get($name) {
-		return ObjectMixin::get($this, $name);
+	public function __get(string $name) {
+		$getter = 'get' . ucfirst($name);
+		if (method_exists($this, $getter)) {
+			return $this->$getter();
+		}
+
+		$getter = 'is' . ucfirst($name);
+		if (method_exists($this, $getter)) {
+			return $this->$getter();
+		}
+		if (property_exists($this, $name)) {
+			return $this->$name;
+		}
+
+		ObjectHelpers::strictGet(get_class($this), $name);
 	}
 
 }
